c70bc51ac7efb0f5d580d2ff6699ea79115e17d7
Alejandro López Martínez <alejandrolm14@gmail.com>
2021-02-19 15:11:13 -0600

    Juego de dado terminado

--- app/src/main/java/alm/itesm/helloworld/MainActivity.kt
+++ app/src/main/java/alm/itesm/helloworld/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("alm.itesm.helloworld", "Demostración de Log")
-        //test_kotlin()
 
         tira_dado()
     }
@@ -19,33 +18,63 @@ class MainActivity : AppCompatActivity() {
     fun tira_dado() {
         var res1 = 0
         var res2 = 0
+        var player = 1
         var turnos = 0
 
-        do {
-            if (turnos % 2 == 0) {
-                mensaje.text = "Total Jugador 1: ${res1}"
-                roll_dice.text = "Jugador 1"
-                roll_dice.setOnClickListener {
+        mensaje.text = "Total Jugador 1: ${res1}"
+        roll_dice.text = "Jugador 1"
+
+        roll_dice.setOnClickListener {
+            if (player == 1) {
+                if(turnos == 6) {
+                    if(res1 > res2) {
+                        mensaje.text = "Jugador 1 gana con ${res1} puntos"
+                        roll_dice.text = "Jugar de nuevo"
+                        res1 = 0
+                        res2 = 0
+                        turnos = 0
+                    }
+                    if(res1 < res2) {
+                        mensaje.text = "Jugador 2 gana con ${res1} puntos"
+                        roll_dice.text = "Jugar de nuevo"
+                        res1 = 0
+                        res2 = 0
+                        turnos = 0
+                    }
+                } else {
                     val rand = Random.nextInt(1, 6)
                     res1 += rand
+                    turnos++
+                    player = 2
+                    mensaje.text = "Total Jugador 1: ${res1}"
+                    roll_dice.text = "Jugador 2"
                 }
-                turnos++
-            } else if (turnos % 2 != 0) {
-                mensaje.text = "Total Jugador 2: ${res1}"
-                roll_dice.text = "Jugador 2"
-                roll_dice.setOnClickListener {
+            } else if (player == 2) {
+                if(turnos == 6) {
+                    if(res1 > res2) {
+                        mensaje.text = "Jugador 1 gana con ${res1} puntos"
+                        roll_dice.text = "Jugar de nuevo"
+                        res1 = 0
+                        res2 = 0
+                        turnos = 0
+                    }
+                    if(res1 < res2) {
+                        mensaje.text = "Jugador 2 gana con ${res1} puntos"
+                        roll_dice.text = "Jugar de nuevo"
+                        res1 = 0
+                        res2 = 0
+                        turnos = 0
+                    }
+                } else {
                     val rand = Random.nextInt(1, 6)
                     res2 += rand
+                    player = 1
+                    mensaje.text = "Total Jugador 2: ${res2}"
+                    roll_dice.text = "Jugador 1"
                 }
-                turnos++
+
             }
-
-        } while (turnos <= 12)
-
-        if (res1 > res2)
-            mensaje.text = "Jugador 1 Ganador"
-        if (res2 > res1)
-            mensaje.text = "Jugador 2 Ganador"
+        }
     }
 }
 
